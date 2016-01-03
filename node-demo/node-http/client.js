49c92f2e3baf75465b6b84cf19644c23698d034e
natumsol <natumsol@gmail.com>
2016-01-03 21:57:35 +0800

    完成server端gzip压缩，client端解压缩的demo

--- node-demo/node-http/client.js
+++ node-demo/node-http/client.js
@@ -1,11 +1,13 @@
 var http = require("http");
+var zlib = require("zlib");
 var options = {
     host: "127.0.0.1",
     port: 8888,
     path: "/",
     method: "POST",
     headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
+        'Accept-Encoding': 'gzip, deflate'
     }
 };
 
@@ -19,8 +21,16 @@ var request = http.request(options, function (res) {
         data.push(chunk);
     })
     res.on("end", function () {
-        data = Buffer.concat(data);
-        console.log(data.toString());
+        if (res.headers["content-encoding"] == 'gzip') {
+            data = Buffer.concat(data);
+            console.log("解压前的数据（" + data.length + "）：" + data.toString());
+            zlib.gunzip(data, function(err, unzipdata){
+                console.log("解压后的数据（" + unzipdata.length + "）：" + unzipdata.toString());
+                console.log("压缩率：" + (1 - data.length / unzipdata.length).toFixed(4) * 100 + "%");
+            });
+        } else {
+             console.log(data.toString("hex"));
+        }
     })
     
     // res.pipe(process.stdout);
